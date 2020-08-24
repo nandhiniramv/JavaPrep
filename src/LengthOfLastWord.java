54829eba404b2483e0cf0be293968ed8f054a632
nandhiniramv <k.nandhini@gmail.com>
2020-08-24 09:58:49 +0800

    Update LengthOfLastWord.java
    cleanup

--- src/LengthOfLastWord.java
+++ src/LengthOfLastWord.java
@@ -1,12 +1,4 @@
 class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        int space=s.trim().lastIndexOf(" ");
-        System.out.println(space);
-       // if (space==s.length()-1)
-        String last = s.trim().substring(space+1);
-        System.out.println(last);
-        return last.length();
-    }
     public static void main(String a[]){
         LengthOfLastWord s = new LengthOfLastWord();
         String st ="Happy Life ";
@@ -14,7 +6,7 @@ class LengthOfLastWord {
         System.out.println(s.lengthOfLastWord(st));
     }
 
-    public int lengthOfLastWord1(String s) {
+    public int lengthOfLastWord(String s) {
         String ts = s.trim();
         String last = ts.substring(ts.lastIndexOf(" ")+1);
         return last.length();
